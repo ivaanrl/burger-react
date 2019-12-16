3a84593a3986d074db20f2b99a60b7c3733be83b
ivaanrl <ivanrl2010@gmail.com>
2019-12-16 11:38:47 -0300

    Add UI for desktop and mobile

--- src/Components/Burger/BuildControls/BuildControls.js
+++ src/Components/Burger/BuildControls/BuildControls.js
@@ -5,7 +5,7 @@ import BuildControl from "./BuildControl/BuildControl";
 const controls = [
   { label: "Salad", type: "salad" },
   { label: "Bacon", type: "bacon" },
-  { label: "Salad", type: "cheese" },
+  { label: "Cheese", type: "cheese" },
   { label: "Meat", type: "meat" }
 ];
 const buildControls = props => (
--- src/Components/Layout/Layout.js
+++ src/Components/Layout/Layout.js
@@ -1,11 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.css";
-const layout = props => (
-  <Aux>
-    <div> Toolbar, sideDrawer, backDrop</div>
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
+import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-export default layout;
+class Layout extends Component {
+  state = {
+    showSideDrawer: false
+  };
+
+  sideDrawerCloseHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+
+  sideDrawerToggle = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar
+          open={this.state.showSideDrawer}
+          drawerToggleClicked={this.sideDrawerToggle}
+        />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+
+export default Layout;
